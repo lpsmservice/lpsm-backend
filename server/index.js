5e0de27da43d103f3ac5c54b5673efc1ee4f53a1
LPSM <SEUEMAIL@gmail.com>
2026-04-20 16:32:52 -0300

    corrige rota painel server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.use('/painel', express.static(path.join(__dirname, '..', 'painel')));
+app.use('/painel', express.static(path.join(__dirname, 'painel')));
 app.use('/downloads', express.static(path.join(__dirname, 'public')));
 
 const uploadDir = path.join(__dirname, 'public', 'uploads');
